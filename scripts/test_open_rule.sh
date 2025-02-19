64d11dcae6fbe49cf753d1265df28467398361ba
xx <1658198604@qq.com>
2025-02-19 20:02:44 +0800

    add logo

--- scripts/test_open_rule.sh
+++ scripts/test_open_rule.sh
@@ -18,16 +18,16 @@ subjects=(
 )
 
 models=(
-    "Meta-Llama-3-8B-Instruct"
+    "Meta-Llama-3-8B-Instruct" # path to llama3-8b
 )
 
 for model in "${models[@]}"; do
     for subject in "${subjects[@]}"; do
-        python generate_open.py --model_path /home/math/models/$model \
+        python generate_open.py --model_path $model \
                                 --subject $subject \
                                 --prompt llama3math \
-                                --tensor_parallel_size 4
-        python eval_rule.py --model_path /home/math/models/$model --subject $subject
+                                --tensor_parallel_size 1
+        python eval_rule.py --model_path $model --subject $subject
     done
-    python eval_rule.py --model_path /home/math/models/$model
+    python eval_rule.py --model_path $model
 done
